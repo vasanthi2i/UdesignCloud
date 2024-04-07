4cb005cf3bc0b50ffe1d06d037f49b305a0491f4
Logeshwaran Sankar <logeshwaran.sankar@ideas2it.com>
2024-04-07 12:57:15 +0530

    Updated the page navigation and buttons files in utils

--- src/Pages/PhotographPage.ts
+++ src/Pages/PhotographPage.ts
@@ -5,22 +5,40 @@ import { Utils } from "../Utils/Utils";
 
 
 //Photographs page
-export class PhotographsPage{
+export class PhotographsPage {
     page: Page;
     uploadCompositeImage: Locator;
-   
+    nextButton: Locator;
+    backButton: Locator;
+    closeButton: Locator;
 
 
-    constructor(page: Page)
-    {
-         this.page= page;
-         this.uploadCompositeImage=page.getByTestId('composite');
+    constructor(page: Page) {
+        this.page = page;
+        this.uploadCompositeImage = page.getByTestId('composite');
+        this.nextButton = page.getByTestId('ChevronRightIcon');
+        this.backButton = page.getByTestId('ChevronLeftIcon');
+        this.closeButton = page.getByRole('button', { name: 'Close' });
     }
 
     // Uploading Composite Image
-    async uploadCompositeImageFile(page: Page): Promise <void>
-    {
-        Utils.uploadSingleFile(this.uploadCompositeImage,'UdesignCloud\src\resources\Photographs\smile.png');
+    async uploadCompositeImageFile(page: Page): Promise<void> {
+        Utils.uploadSingleFile(this.uploadCompositeImage, 'UdesignCloud\src\resources\Photographs\smile.png');
+    }
+
+    //Clicking Next Button
+    async clickNextButtonOnPhotographsPage(page: Page): Promise<void> {
+        Utils.clickNextButton(this.nextButton);
+    }
+
+    // Clicking Close button
+    async clickCloseButtonOnPhotographsPage(): Promise<void> {
+        Utils.clickCloseButton(this.closeButton);
+    }
+
+    // Clicking Back button
+    async clickBackButtonOnPhotographsPage(): Promise<void> {
+        Utils.clickBackButton(this.backButton);
     }
 
 }
